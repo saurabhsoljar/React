25b7c82a036f9ed1a215d97f68cd5af585f2415c
saurabh kumar <93548806+saurabhsoljar@users.noreply.github.com>
2023-08-15 14:53:12 +0530

    Sign Up and Login page
    create Sign Up and Login page  using React

---
+++ login-signup/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import LoginSignup from './components/loginSignup/loginSignup';
+
+function App() {
+  return (
+    <div>
+      <LoginSignup/>
+    </div>
+  );
+}
+
+export default App;
