80badcfa2a6112b0de044458044090e4c648d21b
saurabh kumar <93548806+saurabhsoljar@users.noreply.github.com>
2023-10-10 16:02:30 +0530

    Update App.jsx

--- Food Searching App/food-searching-app/src/App.jsx
+++ Food Searching App/food-searching-app/src/App.jsx
@@ -1,11 +1,21 @@
 
-import './App.css'
-import Navbar from './components/Navbar'
+import React,{ useState } from 'react'
+import Navbar from './components/Navbar' 
 import ProductCard from './components/ProductCard'
 import Searchbar from './components/Searchbar'
 
 function App() {
-   
+  const [allfood, setAllFood] = useState([]);
+  const [search, setSearch] = useState("");
+  const [loading, setLoading]= useState(false);
+
+  const fetchFood = async () => {
+    try {
+      const res = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${search}&app_id=${APP_ID}&app_key=b9e10e10e33ca698c9db3bcfa97abc3f `)
+    } catch (error) {
+      
+    }
+  }
 
   return (
     <>
